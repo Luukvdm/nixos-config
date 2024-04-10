6ac0f1a60fd9072b2abd2991a72ce34aa62fbd71
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2024-04-10 16:08:56 +0200

    updated flakes

--- modules/home-manager/features/neovim/telescope.nix
+++ modules/home-manager/features/neovim/telescope.nix
@@ -3,21 +3,29 @@
     plugins = {
       telescope = {
         enable = true;
-        defaults = {
-          file_ignore_patterns = ["node_modules" "venv" "bin" "obj" ".idea" "vendor"];
+        settings = {
+          defaults = {
+            file_ignore_patterns = ["node_modules" "venv" "bin" "obj" ".idea" "vendor"];
+          };
         };
         keymaps = {
           "<Leader><Space>" = {
             action = "git_files";
-            desc = "Telescope Git Files";
+            options = {
+              desc = "Telescope Git Files";
+            };
           };
           "<Leader>f" = {
             action = "find_files";
-            desc = "Telescope Find Files";
+            options = {
+              desc = "Telescope Find Files";
+            };
           };
           "<Leader>g" = {
             action = "live_grep";
-            desc = "Telescope Live Grep";
+            options = {
+              desc = "Telescope Live Grep";
+            };
           };
         };
         keymapsSilent = true;
