2803627f032f0fac137d49325af5ce52d94f78e1
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2024-07-09 21:29:19 +0200

    update vscode config

--- modules/home-manager/features/vscode.nix
+++ modules/home-manager/features/vscode.nix
@@ -15,10 +15,23 @@
       vscodevim.vim
       kamadorueda.alejandra
       bbenoist.nix
+      editorconfig.editorconfig
+      mkhl.direnv
+      ms-dotnettools.csharp
     ];
+
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
 
     userSettings = {
       update.mode = "none";
+
+      "files.exclude" = {
+        # removes these from the search
+        "**/.direnv" = true;
+        "**/.devenv" = true;
+      };
     };
   };
 }
